a8d2fb9d1a91d95fca2100d3eb507989ba8e76d9
rgabeflores <rgabeflores@gmail.com>
2018-01-03 19:45:50 -0800

    Added Category Count

--- ebay_scrape.py
+++ ebay_scrape.py
@@ -24,13 +24,15 @@ def main():
 
 	items = call_api(keywords=user_input)
 
+	categories = {}
 	prices = []
 
 	for item in items:
-		# category = item.categoryname.string.upper()
-		# title = item.title.string.upper()
+		if item.categoryname.string.upper() in categories:
+			categories[item.categoryname.string.upper()] += 1
+		else:
+			categories[item.categoryname.string.upper()] = 1
 		prices.append(round(float(item.currentprice.string), 2))
-		# url = item.viewitemurl.string
 
 	if len(prices) > 0:
 		prices.sort()
@@ -43,15 +45,20 @@ def main():
 		std_dev = round(stats.std_dev(prices, average=mean, var=variance), 2)
 
 		print("\t" + ("_____" * 10) + "\n")
-		print("\t\tHigh", high)
-		print("\t\tLow", low)
-		print("\t\tMean:", mean)
-		print("\t\tMedian:", median)
-		print("\t\tMode:", mode)
-		print("\t\tVariance:", variance)
-		print("\t\tStandard Deviation:", std_dev)
+		print("\tFound " + str(len(prices)) + " different prices.")
+		for key in categories:
+			print("\tFound " + str(categories[key]) + " in " + key + ".")
 		print("\t" + ("_____" * 10) + "\n")
 
+		print("\tHigh:", high)
+		print("\tLow:", low)
+		print("\tMean:", mean)
+		print("\tMedian:", median)
+		print("\tMode:", mode)
+		print("\tVariance:", variance)
+		print("\tStandard Deviation:", std_dev)
+
+		print("\t" + ("_____" * 10) + "\n")
 		print("\tApproximately 68%% of offers are between", round(mean - std_dev, 2),"and", round(mean + std_dev, 2))
 		print("\tApproximately 95%% of offers are between", round(mean - (2 * std_dev), 2),"and", round(mean + (2 * std_dev), 2))
 		print("\tApproximately 99.7%% of offers are between", round(mean - (3 * std_dev), 2),"and", round(mean + (3 * std_dev), 2))
