2a26122fe2fe5b47a74da9a7bec92847ad91b8fe
rgabeflores <rgabeflores@gmail.com>
2018-01-26 11:52:11 -0800

    Added Test Module

---
+++ test_ebay_scrape.py
@@ -0,0 +1,12 @@
+import unittest
+import ebay_scrape
+
+
+class TestEbayScrape(unittest.TestCase):
+
+    def test_apicall(self):
+        result = call_ebay_api()
+
+
+if __name__ == "__main__":
+    unittest.main()
